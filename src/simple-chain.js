71435a0b54bba220e1227529f848d665ba0fdd1f
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-04-21 18:26:41 +0300

    feat: end task simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,28 +5,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  string: '',
+  reset: '',
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.string.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) { // цепочность возможна, если каждый метод возвращает сам объект
+    this.string +=`( ${value} )~~`;
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    this.string = this.string.slice(0, -2).split(`~~`); // удалять хвост
+    if (position <= 0 || position > this.string.length || typeof(position) !== 'number') {
+      this.string = '';
+      throw new Error('You can\'t remove incorrect link!');
+    }
+    this.string = this.string.filter((item, idx, array) => {
+      if (position - 1 !== idx) {
+        return item;
+      }
+    }).join('~~') + '~~';
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.string) {
+      this.string = this.string.slice(0, -2); // удалять хвост
+      this.string = this.string.split('~~').reverse().join('~~') + '~~';
+    }
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.reset = this.string.slice(0, -2);
+    this.string = '';
+    return this.reset;
   }
 };
 
 module.exports = {
   chainMaker
 };
+
+// npm run test ./test/simple-chain.test.js
