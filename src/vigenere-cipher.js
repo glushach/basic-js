794a911df20f7a0282836007da664d2efd58e066
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-04-26 09:08:58 +0300

    feat: end task vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,74 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(route) {
+    if (route === false) {
+      this.route = "reverse";
+    } else {
+      this.route = "direct";
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  ciphering(message, key, method) { // method равен "encrypt" или "decrypt"
+    const encrypted = [];
+    message = message.toLowerCase().split('');
+    message.forEach(item => {
+      if (item >= "a" && item <= "z") {
+        encrypted.push(item.charCodeAt(0));
+      } else {
+        encrypted.push(item);
+      }
+    });
+    // console.log('Encrypted', encrypted)
+
+    const keys = key.toLowerCase().split('').map(item => item.charCodeAt(0) - "a".charCodeAt(0));
+    const result = [];
+    let count = 0;
+
+    encrypted.forEach((item, idx, arr) => {
+      if (count === keys.length) count = 0;
+      if (typeof(item) !== 'number') {
+        result.push(item);
+        return; // пропуск итерации
+      }
+
+      let char = '';
+      if (method === 'encrypt') {
+        char = item + keys[count];
+        if (char > "z".charCodeAt(0)) char -= 26;
+      } else {
+        char = item - keys[count];
+        if (char < "a".charCodeAt(0)) char += 26;
+      }
+      count++;
+      result.push(String.fromCharCode(char));
+    });
+
+    return result.join('').toUpperCase();
+
+  }
+
+  encrypt(string, key) { // прямой шифровать
+    // string(для кодирования) и key(string-ключевое слово)
+    if (!(string && key)) throw new Error('Incorrect arguments!');
+
+    if (this.route === 'direct') return this.ciphering(string, key, 'encrypt');
+
+    let reverse = this.ciphering(string, key, 'encrypt');
+    return reverse.split('').reverse().join('');
+  }
+  decrypt(string, key) { // реверсный расшифровать
+    // string(для декодирования) и key( string-ключевое слово).
+    if (!(string && key)) throw new Error('Incorrect arguments!');
+
+    if (this.route === 'direct') return this.ciphering(string, key, 'decrypt');
+
+    let reverse = this.ciphering(string, key, 'decrypt');
+    return reverse.split('').reverse().join('');
   }
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
+
+// npm run test ./test/vigenere-cipher.test.js
